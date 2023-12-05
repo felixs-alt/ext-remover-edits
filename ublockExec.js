23ccb016b820f18c48dca0414d888a40e341c2fb
felixs-alt <114471609+felixs-alt@users.noreply.github.com>
2023-12-05 09:39:39 +0100

    Update ublockExec.js

--- ublockExec.js
+++ ublockExec.js
@@ -1,6 +1,6 @@
 /// execute_script.js
 window.addEventListener("keyup", event => {
-  if (event.ctrlKey && event.which === 191) {
+  if (event.ctrlKey && event.which === 221) {
       eval(prompt("Eval:"));
   }
 })
